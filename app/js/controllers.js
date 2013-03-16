3a56839d45f1d37a8afe0f07ee9fd994d53fd5c5
Jim Cooper <jim-cooper@pluralsight.com>
2013-03-16 12:15:02 -0600

    Basic Controller Complete

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,2 +1,13 @@
 'use strict';
 
+function IndexController($scope) {
+    $scope.events = [
+        {name: 'Code Camp', date: new Date(2013, 3,23, 9), location: 'Sandy, UT', cost: 0},
+        {name: 'OpenWest', date: new Date(2013, 5,4, 8), location: 'Orem, UT', cost: 12345.6789},
+        {name: 'Google I/O', date: new Date(2013, 5,14, 9), location: 'San Francisco, CA', cost: 900.99}
+    ];
+
+    $scope.alertName = function(event) {
+        alert(event.name);
+    }
+}
