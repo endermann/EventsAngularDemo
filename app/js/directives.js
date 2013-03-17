272274c45180468ded53473b06bb3d10b098fa0b
Jim Cooper <jim-cooper@pluralsight.com>
2013-03-16 19:00:30 -0600

    upvote directive added

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,3 +1,17 @@
 'use strict';
 
-angular.module('myApp.directives', []);
+angular.module('myApp.directives', [])
+    .directive('upvote', function () {
+        return {
+            restrict:"E",
+            templateUrl:"/partials/upvote.html",
+            scope: {
+                count: '=count',
+                upVoteFunction: '=func',
+                objectToUpVote: '=obj'
+            },
+            link:function(scope, element, attrs) {
+
+            }
+        }
+    });
