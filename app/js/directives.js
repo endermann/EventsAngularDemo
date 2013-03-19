230a5eadb05e66ed0284e51ac586e5e08ce49999
Jim Cooper <jim-cooper@pluralsight.com>
2013-03-18 21:24:19 -0600

    Upvote Directive (Bad)

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,3 +1,9 @@
 'use strict';
 
-angular.module('myApp.directives', []);
+angular.module('myApp.directives', [])
+    .directive('upvote', function() {
+       return {
+         restrict: 'E',
+         templateUrl: '/partials/upvote.html'
+       };
+    });
