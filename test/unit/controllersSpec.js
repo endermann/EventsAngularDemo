52cb35e6c3d88b0db44a3a124b3da1ed8f345337
Jim Cooper <jim-cooper@pluralsight.com>
2013-03-20 23:51:23 -0600

    First Test

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,2 +1,26 @@
 'use strict';
 
+describe('IndexController', function () {
+    var controller;
+    var scope;
+
+    beforeEach(function () {
+        module('myApp');
+        inject(function ($rootScope, $controller, eventData) {
+            scope = $rootScope.$new();
+            controller = $controller(IndexController, {$scope: scope, eventData: eventData});
+        })
+    });
+
+    describe('when editing the name', function () {
+        it('should edit the name on the event', function () {
+            var event = {name: "Code Camp"};
+
+            scope.editName(event);
+
+            expect(event.name).toEqual('foo');
+        });
+    });
+});
+
+
