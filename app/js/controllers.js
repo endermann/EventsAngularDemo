7e93934b932b4b8d81c9e13e98b59c1ad925f676
Jim Cooper <jim-cooper@pluralsight.com>
2013-03-20 23:31:38 -0600

    EventData Service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,7 @@
 'use strict';
 
-function IndexController($scope) {
-    $scope.events = [
-        {name: 'Code Camp', date: new Date(2013, 3,23, 9), location: 'Sandy, UT', cost: 0},
-        {name: 'OpenWest', date: new Date(2013, 5,4, 8), location: 'Orem, UT', cost: 12345.6789},
-        {name: 'Google I/O', date: new Date(2013, 5,14, 9), location: 'San Francisco, CA', cost: 900.99}
-    ];
+function IndexController($scope, eventData) {
+    $scope.events = eventData.events;
 
     $scope.editName = function(event) {
         event.name = "foo";
