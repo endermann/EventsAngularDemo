41c033907111f0abb809dca0a2332b0269fe7b50
Jim Cooper <jim-cooper@pluralsight.com>
2013-03-21 01:24:17 -0600

    filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,3 +1,16 @@
 'use strict';
 
-angular.module('myApp.filters', []);
+angular.module('myApp.filters', [])
+    .filter('truncate', function () {
+        return function (text, length) {
+            if (!length)
+                length = 50;
+
+            if (text.length <= length) {
+                return text;
+            }
+            else {
+                return text.substring(0, length) + "...";
+            }
+        };
+    });
