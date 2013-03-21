4af798cdfc09fd2cc4ae0469bb7b7d9a6f4d2d8e
Jim Cooper <jim-cooper@pluralsight.com>
2013-03-21 00:36:31 -0600

    upvoting

--- app/js/app.js
+++ app/js/app.js
@@ -4,5 +4,7 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives'])
     .config(function ($routeProvider, $locationProvider) {
         $routeProvider.when('/events', {templateUrl: '/partials/eventList.html', controller: EventListController })
         $routeProvider.otherwise({redirectTo: '/events'});
+        $routeProvider.when('/event/:eventId', {templateUrl: '/partials/event.html', controller: EventController})
+        
         $locationProvider.html5Mode(true);
     });
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,23 @@
 'use strict';
 
-function EventListController($scope, eventData) {
+function EventListController($scope, eventData, $location) {
     $scope.events = eventData.events;
+
+    $scope.showEvent = function(event) {
+        $location.url('event/' + event.id);
+    }
 
     $scope.editName = function(event) {
         event.name = "foo";
     }
 }
+
+function EventController($scope, $routeParams, eventData) {
+    $scope.event = _.findWhere(eventData.events, {id: parseInt($routeParams.eventId)});
+
+    $scope.upVoteSession = function(session) {
+        session.upVoteCount++;
+    }
+}
+
+
